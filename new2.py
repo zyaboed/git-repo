fb9922fc743036538eb47809d7b4276ed8bba286
zyaboed <zyaboed@gmail.com>
2019-07-09 23:25:04 +0300

    Ну,  надо же что-то засунуть сюда

---
+++ new2.py
@@ -0,0 +1,8 @@
+print('Привет!')
+name = input('Введите ваше имя: ')
+print(name, ', привет!')
+answer = input('Поработаем? Y или N? - ')
+if answer == 'Y':
+    print("Молодец! Работай")
+else:
+    print('Ну и фиг с тобой, жаба')    
